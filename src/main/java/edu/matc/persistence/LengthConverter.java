6f5dcd4f915dcab0f8267ef789374827ab104102
Joanne Saunders <jasaunders@madisoncollege.edu>
2018-04-02 19:46:43 -0500

    wrote logic for converting inches. Set up main flow through class in convertUnits method. Wrote palin text response logic. Sketch in logic and methods for html and json responses. Started logic for converting feet. Tested through URI with http://localhost:8080/converter/services/length/text/in/ft/12. Successfully  return response.

--- src/main/java/edu/matc/persistence/LengthConverter.java
+++ src/main/java/edu/matc/persistence/LengthConverter.java
@@ -10,42 +10,151 @@ import javax.ws.rs.core.Response;
 
 @Path("/length")
 public class LengthConverter {
+
+    private Double convertedQuantity = 0.0;
+    private String output;
+
+
     // The Java method will process HTTP GET requests
     @GET
-    @Path("/text/{params}")
-    // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
-    public Response getLengthText(@PathParam("params") String name) {
-        // Return a simple message
-      /*  switch (inUints) {
-            case ("in") : //some code
-                break;
-        }*/
+    @Path("/text/{inUnit}/{outUnit}/{quantity}")
+    public Response getLengthText(
+            @PathParam("inUnit") String inUnit,
+            @PathParam("outUnit") String outUnit,
+            @PathParam("quantity") double quantity) {
 
-       // return Response.status(200).entity(output).build();
+        convertUnits(inUnit, outUnit, quantity);
+        output += quantity + " " + inUnit + " =  " + convertedQuantity + " " + outUnit; // call format text function
 
-        return null;
+        return Response.status(200).entity(output).build();
     }
 
+
     @GET
-    @Path("/html/{param}")
     @Produces("text/html")
-    public Response getLengthHTML(@PathParam("param") String name) {
-        //return message as html
-        String output = "<html> <title>HTML from HelloWorld</title> "
-                + "<body><h1>Hello from length html method"
+    @Path("/html/{inUnit}/{outUnit}/{quantity}")
+    public Response getLengthHTML(
+            @PathParam("inUnit") String inUnit,
+            @PathParam("outUnit") String outUnit,
+            @PathParam("quantity") double quantity) {
+
+        convertUnits(inUnit, outUnit, quantity);
+        String output = "<html> <title>length converter</title> "
+                + "<body><h1>"
+                + quantity
+                + " "
+                + inUnit
+                + " = "
+                + convertedQuantity
+                + " "
+                + outUnit
                 + "</h1></body></html>";
         return Response.status(200).entity(output).build();
 
     }
 
     @GET
-    @Path("/json/{param}")
     @Produces("text/json")
-    public Response getLengthJSON(@PathParam("param") String name) {
-        //return message as html
+    @Path("/json/{inUnit}/{outUnit}/{quantity}")
+    public Response getLengthJSON(
+            @PathParam("inUnit") String inUnit,
+            @PathParam("outUnit") String outUnit,
+            @PathParam("quantity") double quantity) {
+
+        convertUnits(inUnit, outUnit, quantity);
         String output = "Hello  from length/json method";
         return Response.status(200).entity(output).build();
 
     }
+
+    private void convertUnits(String inUnit, String outUnit, Double quantity) {
+        switch (inUnit) {
+            case ("in") : convertInches(quantity, outUnit);
+                break;
+            case ("ft") : convertFeet(quantity, outUnit);
+                break;
+            case ("yard") : convertYard(quantity, outUnit);
+                break;
+            case ("mile") : convertMile(quantity, outUnit);
+                break;
+            case ("mm") : convertMM(quantity, outUnit);
+                break;
+            case ("km") : convertKM(quantity, outUnit);
+                break;
+            default : output = "error converting inches in plain text";
+        }
+    }
+
+    private void convertInches (Double quantity, String outUnit) {
+
+        switch (outUnit) {
+
+            case ("ft") : convertedQuantity = (quantity / 12);
+                break;
+            case ("yard") : convertedQuantity = (quantity / 36);
+                break;
+            case ("mile") : convertedQuantity = (quantity / 63360);
+                break;
+            case ("mm") : convertedQuantity = (quantity * 25.4);
+                break;
+            case ("cm") : convertedQuantity = (quantity * 2.54);
+                break;
+            case ("m") : convertedQuantity = (quantity * .0254);
+                break;
+            case ("km") : convertedQuantity = (quantity * 2.54e-5);
+                break;
+            default : output = "error converting inches";
+        }
+
+    }
+
+    private void convertFeet (Double quantity, String outUnit) {
+        switch (outUnit) {
+            case ("in"):
+                convertedQuantity = quantity * 12;
+                break;
+            case ("yard"):
+                convertedQuantity = quantity / 3;
+                break;
+            case ("mile"):
+                convertedQuantity = quantity / 1000; // need to reassess this formula
+                break;
+            case ("mm"):
+                convertedQuantity = quantity; // need formula
+                break;
+            case ("cm"):
+                convertedQuantity = quantity; // need formula
+                break;
+            case ("m"):
+                convertedQuantity = quantity; // need formula
+                break;
+            case ("km"):
+                convertedQuantity = quantity; // need formula
+                break;
+            default:
+                output = "error converting ft";
+        }
+
+    }
+
+    private void convertYard (Double quantity, String outUnit) {
+
+        convertedQuantity = 0.0;
+    }
+    private void convertMile (Double quantity, String outUnit) {
+
+        convertedQuantity = 0.0;
+    }
+
+    private void convertMM (Double quantity, String outUnit) {
+
+        convertedQuantity = 0.0;
+    }
+
+    private void convertKM (Double quantity, String outUnit) {
+
+        convertedQuantity = 0.0;
+    }
+
 }
